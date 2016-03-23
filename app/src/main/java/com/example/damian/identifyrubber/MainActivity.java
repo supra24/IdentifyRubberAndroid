3ab4805132ee0b5fb3b4b8ff71289d8dc1dc4792
Damian Supera <tojota24@gmail.com>
2016-03-23 21:30:42 +0100

    poprawa select picture

--- app/src/main/java/com/example/damian/identifyrubber/MainActivity.java
+++ app/src/main/java/com/example/damian/identifyrubber/MainActivity.java
@@ -2,16 +2,21 @@ package com.example.damian.identifyrubber;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final int CAMERA_REQUEST = 1;
-    private int PICK_IMAGE_REQUEST = 20;
+    private int PICK_IMAGE_REQUEST = 1;
     private ImageView imgViewPhoto;
 
     @Override
@@ -28,11 +33,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void ActionButtonSelectPicture (View view){
-        Intent intent = new Intent();
+        Intent intent = new Intent(Intent.ACTION_PICK);
 
         intent.setType("image/*"); // wybierz zdjęcie ze wszysktkich rozszezeń
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(intent, PICK_IMAGE_REQUEST);
+
     }
     public void ActionButtonTakePicture ( View view){
 //        Intent cameraIntent = new Intent();
@@ -51,9 +57,22 @@ public class MainActivity extends AppCompatActivity {
 
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK) {
 
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imgViewPhoto.setImageBitmap(imageBitmap);
+            Uri imageUri = data.getData();
+            InputStream imputStream;
+
+            try {
+                imputStream = getContentResolver().openInputStream(imageUri);
+
+                Bitmap bitmap = BitmapFactory.decodeStream(imputStream);
+                imgViewPhoto.setImageBitmap(bitmap);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            imgViewPhoto.setImageBitmap(imageBitmap);
 //            imgViewPhoto.setEnabled(true);
         }
     }
