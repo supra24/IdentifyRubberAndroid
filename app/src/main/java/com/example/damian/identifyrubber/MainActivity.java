1c88a8f9ffd8cf2d34414933c9bfaea429dc88b5
Damian Supera <tojota24@gmail.com>
2016-03-25 13:43:11 +0100

    dodanie możliwości obrotu telefonu

--- app/src/main/java/com/example/damian/identifyrubber/MainActivity.java
+++ app/src/main/java/com/example/damian/identifyrubber/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.damian.identifyrubber;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Parcelable;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,11 +16,12 @@ import android.widget.ImageView;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    public static final int CAMERA_REQUEST = 1;
+    public static final int CAMERA_REQUEST = 10;
     private int PICK_IMAGE_REQUEST = 1;
-    private ImageView imgViewPhoto;
+    private ImageView imgViewPhoto=null;
+    private Uri imageUri=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,39 +45,55 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void ActionButtonTakePicture ( View view){
-//        Intent cameraIntent = new Intent();
-//        File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-//        String pictureDirectoryPath = pictureDirectory.getPath();
-//
-//        Uri data = Uri.parse(pictureDirectoryPath);
-//        cameraIntent.setType("image/*"); // wybierz zdjęcie ze wszysktkich rozszezeń
-//        cameraIntent.setAction(Intent.ACTION_GET_CONTENT);
-//        if (cameraIntent.resolveActivity(getPackageManager()) != null)
-//            startActivityForResult(cameraIntent, CAMERA_REQUEST);
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK) {
+        if ((requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK) || (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK)){
 
-            Uri imageUri = data.getData();
+            imageUri = data.getData();
             InputStream imputStream;
 
             try {
                 imputStream = getContentResolver().openInputStream(imageUri);
-
                 Bitmap bitmap = BitmapFactory.decodeStream(imputStream);
                 imgViewPhoto.setImageBitmap(bitmap);
 
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
-
-//            Bundle extras = data.getExtras();
-//            Bitmap imageBitmap = (Bitmap) extras.get("data");
-//            imgViewPhoto.setImageBitmap(imageBitmap);
-//            imgViewPhoto.setEnabled(true);
         }
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState.getBoolean("wasImage") == true) {
+            imgViewPhoto.setImageBitmap((Bitmap) savedInstanceState.getParcelable("image"));
+            imageUri = savedInstanceState.getParcelable("imageUri");
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+//        outState.putParcelable("image", outState.getParcelable("image"));
+        InputStream imputStream;
+        if ( (imageUri!=null)) {
+            // (imgViewPhoto != null) &&
+            outState.putParcelable("imageUri", imageUri);
+            try {
+                imputStream = getContentResolver().openInputStream(imageUri);
+                Bitmap bitmap = BitmapFactory.decodeStream(imputStream);
+                outState.putParcelable("image", bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        outState.putBoolean("wasImage", (imageUri!=null));
     }
 }
