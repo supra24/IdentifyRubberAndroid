e5397160c0edfc0195b52a489542eedf750b22e0
Damian Supera <tojota24@gmail.com>
2016-03-26 20:57:13 +0100

    poprawa obrotu telefonem

--- app/src/main/java/com/example/damian/identifyrubber/MainActivity.java
+++ app/src/main/java/com/example/damian/identifyrubber/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
 import android.net.Uri;
 import android.os.Parcelable;
 import android.provider.MediaStore;
@@ -21,14 +22,12 @@ public class MainActivity extends Activity {
     public static final int CAMERA_REQUEST = 10;
     private int PICK_IMAGE_REQUEST = 1;
     private ImageView imgViewPhoto=null;
-    private Uri imageUri=null;
     private Button bSelectPicture, bTakePicture, bRun;
+    private Bitmap bitmap1 = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
         bSelectPicture = (Button) findViewById(R.id.buttonSelectPicture);
         bTakePicture = (Button) findViewById(R.id.buttonTakePicture);
@@ -45,6 +44,7 @@ public class MainActivity extends Activity {
         startActivityForResult(intent, PICK_IMAGE_REQUEST);
 
     }
+
     public void ActionButtonTakePicture ( View view){
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
@@ -53,6 +53,10 @@ public class MainActivity extends Activity {
 
     public void ActionButtonRun(View view){
 
+        GreyScale greyScale = new GreyScale(bitmap1);
+        greyScale.Edit();
+        bitmap1 = greyScale.getImage();
+        imgViewPhoto.setImageBitmap(bitmap1);
     }
 
     @Override
@@ -60,46 +64,42 @@ public class MainActivity extends Activity {
 
         if ((requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK) || (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK)){
 
-            imageUri = data.getData();
+            Uri imageUri = data.getData();
             InputStream imputStream;
 
             try {
                 imputStream = getContentResolver().openInputStream(imageUri);
                 Bitmap bitmap = BitmapFactory.decodeStream(imputStream);
-                imgViewPhoto.setImageBitmap(bitmap);
+                bitmap1 = Bitmap.createScaledBitmap(bitmap, 816, 612, false);
+                imgViewPhoto.setImageBitmap(bitmap1);
 
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
-            bRun.setVisibility(View.VISIBLE);
 
+            bRun.setVisibility(View.VISIBLE);
         }
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
+
         if (savedInstanceState.getBoolean("wasImage") == true) {
-            imgViewPhoto.setImageBitmap((Bitmap) savedInstanceState.getParcelable("image"));
-            imageUri = savedInstanceState.getParcelable("imageUri");
+            bitmap1 = savedInstanceState.getParcelable("image");
+            imgViewPhoto.setImageBitmap(bitmap1);
             bRun.setVisibility(View.VISIBLE);
         }
+
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        InputStream imputStream;
-        if ( (imageUri!=null)) {
-            outState.putParcelable("imageUri", imageUri);
-            try {
-                imputStream = getContentResolver().openInputStream(imageUri);
-                Bitmap bitmap = BitmapFactory.decodeStream(imputStream);
-                outState.putParcelable("image", bitmap);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
+
+        if ( (bitmap1!=null)) {
+            outState.putParcelable("image", bitmap1);
         }
-        outState.putBoolean("wasImage", (imageUri!=null));
+        outState.putBoolean("wasImage", (bitmap1!=null));
     }
 }
